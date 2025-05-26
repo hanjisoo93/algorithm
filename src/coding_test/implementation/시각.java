dbf2f51974b00ce8d5e594366a2d5c8f8d955c36
Jisoo Han <hanjisoo93@gmail.com>
2025-05-26 23:17:38 +0900

    feat: 0526 [Implementation] 시각 (이코테) - 리팩토링

--- src/coding_test/implementation/시각.java
+++ src/coding_test/implementation/시각.java
@@ -15,10 +15,9 @@ public class 시각 {
         for(int h = 0; h < n + 1; h++) {
             for(int m = 0; m < 60; m ++){
                 for(int s = 0; s < 60; s ++){
-                    if(String.valueOf(h).contains(Integer.toString(n)) ||
-                        String.valueOf(m).contains(Integer.toString(n))||
-                        String.valueOf(s).contains(Integer.toString(n))) {
-                       result ++;
+                    String time = String.valueOf(h) + String.valueOf(m) + String.valueOf(s);
+                    if(time.contains(Integer.toString(n))) {
+                        result ++;
                     }
                 }
             }
